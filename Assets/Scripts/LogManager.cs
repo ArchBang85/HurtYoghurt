30a6f2e0a38d95686e1d9311fb434460e4434f7a
Petri Autio <archbang85@gmail.com>
2015-03-11 22:24:59 +0000

    Message log system begun

---
+++ Assets/Scripts/LogManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class LogManager : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public GameObject scrollLog;
+    public List<string> logMessages;
+    private string[] activeLogMessages = new string[5];
+    public GameObject[] activeLogMessageStrips = new GameObject[5];
+
+
+    // Use this for initialization
+    void Start()
+    {
+        scrollLog = GameObject.Find("ScrollLog");
+
+        logMessages.Clear();
+        for (int v = 0; v < 5; v++)
+        {
+            logMessage("hello " + v);
+        }
+        foreach(string s in activeLogMessages)
+        {
+            Debug.Log(s);
+        }
+        logMessage("hello new");
+        foreach (string s in activeLogMessages)
+        {
+            Debug.Log(s);
+        }
+   
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            scrollLog.transform.Rotate(-Vector3.right, 250 * Time.deltaTime, Space.World);
+        }
+
+    }
+
+    // A method that takes new messages and pops it into the array of messages, rotates the roll, removes old messages and adds the new one to the top
+    void logMessage(string s)
+    {
+        // keep complete log
+        logMessages.Add(s);
+        
+        // Establish length of log
+        // 
+        int maxLineLength = 26;
+        bool multiLine = false;
+        if(s.Length > maxLineLength)
+        {
+            // need to split across multiple lines
+            multiLine = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(i % maxLineLength == 0)
+                {
+                    string s1 = s.Substring(0, maxLineLength);
+                    string s2 = s.Substring(maxLineLength + 1, s.Length);
+                }
+            }
+        }
+
+        // rotate log
+       
+        
+        // update active messages 
+        for (int i = 4; i > 0; i--)
+        {
+            activeLogMessages[i] = activeLogMessages[i - 1];
+        }
+
+        activeLogMessages[0] = s;
+
+
+    }
+}
---
+++ Assets/Scripts/Rotations.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotations : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void rotateLog()
+    {
+        transform.Rotate(-Vector3.right, speed * Time.deltaTime, Space.World);
+        Debug.Log("rotating");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /*
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.Rotate(-Vector3.right, speed * Time.deltaTime, Space.World);
+
+    }*/
+    }
+}
