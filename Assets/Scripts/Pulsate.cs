77e89763096b544354c190471522c3f2c2e87f94
Petri Autio <archbang85@gmail.com>
2015-03-12 23:01:10 +0000

    Player and mask art

---
+++ Assets/Scripts/Pulsate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulsate : MonoBehaviour {
+
+    public float minIntensity = 0.4f;
+    public float maxIntensity = 1.6f;
+    private bool goingUp = true;
+    public float speed = 0.01f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(goingUp && this.transform.light.intensity < maxIntensity)
+        {
+            this.transform.light.intensity += Time.deltaTime * speed;
+        } else if (this.transform.light.intensity >= maxIntensity)
+        {
+            goingUp = false;
+            this.transform.light.intensity -= Time.deltaTime * speed;
+        } else if (!goingUp)
+        {
+            this.transform.light.intensity -= Time.deltaTime * speed;
+        } 
+        
+        if (this.transform.light.intensity <= minIntensity)
+        {
+            goingUp = true;
+            this.transform.light.intensity += Time.deltaTime * speed;
+        }
+
+	}
+}
